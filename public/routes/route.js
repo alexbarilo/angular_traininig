a68013c9ed7fdb38259546ce9e050982f980bfcb
DSK8 DSK <DSK8@ptc.local>
2016-12-01 17:55:20 +0200

    las stable version

--- public/routes/route.js
+++ public/routes/route.js
@@ -13,6 +13,9 @@ module.config(
         }).when('/section:name', {
             templateUrl: 'routes/viewSection/viewSection.html',
             controller: 'ViewSectionController'
+        }).when('/register', {
+            templateUrl: 'routes/userForm/userForm.html',
+            controller: 'UserFormController'
         }).when('/:section?', {
             templateUrl: 'routes/notes/notes.html',
             controller: 'NotesController'
